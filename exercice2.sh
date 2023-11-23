4b665fbc538d11ba3a9fb9cb2c9f7a2611059aa5
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2023-11-23 20:16:14 +0100

    fixing bugs

--- exercice2.sh
+++ exercice2.sh
@@ -3,13 +3,13 @@ echo -n "Entrez la note : "
 read note
 if [ $note -ge 0 ] && [ $note -lt 10 ]; then
 	echo "insuffisant"
-elif [ $note -lt 12 ]; then
+elif [ $note -ge 10 ] && [ $note -lt 12 ]; then
 	echo "moyen"
-elif [ $note -lt 14 ]; then
+elif [ $note -ge 12 ] && [ $note -lt 14 ]; then
 	echo "assez bien"
-elif [ $note -lt 16 ] ; then
+elif [ $note -ge 14 ] && [ $note -lt 16 ] ; then
         echo "bien"
-elif [ $note -le 20 ] ; then
+elif [ $note -ge 16 ] && [ $note -le 20 ] ; then
         echo "très bien"
 else 
 	echo "note n'est pas valide"
